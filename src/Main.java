1bc6f280396b2a4ff4e3e1fdd0979d91fb82e8da
SQA-SOLOMAN <kavkazus@hotmail.com>
2023-08-17 09:00:24 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+//    public static void main(String[] args) {
+//        String name;
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("What's your name? ");
+//        name = sc.nextLine();
+//        System.out.println(name);
+//    }
+
+    public static void main(String[] args) {
+
+//        int userInput;
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Whats your weight \n");
+//        userInput = sc.nextInt();
+//        double myConverter = .45359237;
+//        System.out.println("The naswer is " + userInput * myConverter);
+//
+//        char SimpleCHar = '?';
+//        char myUnicder = '\u003F';
+//        char MyDecimalChar = 63;
+//        System.out.println(SimpleCHar);
+//        System.out.println(myUnicder);
+//        System.out.println(MyDecimalChar);
+    }
+}
